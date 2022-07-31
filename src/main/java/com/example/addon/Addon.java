0e952f34f2989d8a7b03e0b851b33077802be2e2
RacoonDog <32882447+RacoonDog@users.noreply.github.com>
2022-07-31 18:12:20 -0400

    Use LogUtils.getLogger()

--- src/main/java/com/example/addon/Addon.java
+++ src/main/java/com/example/addon/Addon.java
@@ -3,6 +3,7 @@ package com.example.addon;
 import com.example.addon.commands.CommandExample;
 import com.example.addon.hud.HudExample;
 import com.example.addon.modules.ModuleExample;
+import com.mojang.logging.LogUtils;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
 import meteordevelopment.meteorclient.systems.commands.Commands;
 import meteordevelopment.meteorclient.systems.hud.Hud;
@@ -10,10 +11,9 @@ import meteordevelopment.meteorclient.systems.hud.HudGroup;
 import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Addon extends MeteorAddon {
-    public static final Logger LOG = LoggerFactory.getLogger(Addon.class);
+    public static final Logger LOG = LogUtils.getLogger();
     public static final Category CATEGORY = new Category("Example");
     public static final HudGroup HUD_GROUP = new HudGroup("Example");
 
