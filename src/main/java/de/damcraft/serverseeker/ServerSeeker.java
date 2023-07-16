324764cf355336f1d0ad022b0fe04aa24fef04ae
DAM <43420467+DAMcraft@users.noreply.github.com>
2023-07-16 12:24:50 +0200

    Added feature list for anticope.ml

--- src/main/java/de/damcraft/serverseeker/ServerSeeker.java
+++ src/main/java/de/damcraft/serverseeker/ServerSeeker.java
@@ -5,6 +5,14 @@ import meteordevelopment.meteorclient.addons.MeteorAddon;
 import org.slf4j.Logger;
 
 public class ServerSeeker extends MeteorAddon {
+    /*
+    Feature list for anticope.ml:
+    (creates features matching the RegEx '(?:add\(new )([^(]+)(?:\([^)]*)\)\)', as anticope checks for that.
+    add(new Find servers with many parameters, for example: Cracked, Description, Player count, much more...())
+    add(new Server database with around 1.000.000 servers!())
+    add(new Over 40.000.000 players tracked!())
+    add(new Search for ANY server you want!())
+     */
     public static final Logger LOG = LogUtils.getLogger();
 
     @Override
