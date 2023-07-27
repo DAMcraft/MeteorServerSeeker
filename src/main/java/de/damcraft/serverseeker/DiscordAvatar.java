e01f8f8bdfcbac84098615490ee418ba83d856ee
DAM <43420467+DAMcraft@users.noreply.github.com>
2023-07-27 07:06:51 +0200

    Fixed a bug where the game would crash if a user changed their discord pfp.

--- src/main/java/de/damcraft/serverseeker/DiscordAvatar.java
+++ src/main/java/de/damcraft/serverseeker/DiscordAvatar.java
@@ -1,20 +1,47 @@
 package de.damcraft.serverseeker;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.mojang.blaze3d.systems.RenderSystem;
 import meteordevelopment.meteorclient.renderer.Texture;
+import meteordevelopment.meteorclient.systems.Systems;
 import meteordevelopment.meteorclient.utils.network.Http;
 import org.lwjgl.BufferUtils;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.ByteBuffer;
 
 public class DiscordAvatar extends Texture {
     public DiscordAvatar(String url) {
         BufferedImage avatar;
         try {
-            avatar = ImageIO.read(Http.get(url).sendInputStream());
+            InputStream stream = Http.get(url).sendInputStream();
+            if (stream == null) {
+                JsonObject params = new JsonObject();
+
+                params.addProperty("api_key", Systems.get(ServerSeekerSystem.class).apiKey);
+
+                String jsonResp = SmallHttp.post("https://api.serverseeker.net/user_info", params.toString());
+
+                JsonObject obj = (new Gson()).fromJson(jsonResp, JsonObject.class);
+                if (obj.has("error")) {
+                    System.out.println("Error: " + obj.get("error").getAsString());
+                    return;
+                }
+                String discordId = obj.get("discord_id").getAsString();
+                String discordUsername = obj.get("discord_username").getAsString();
+                String discordAvatarUrl = obj.get("discord_avatar_url").getAsString();
+
+                Systems.get(ServerSeekerSystem.class).discordId = discordId;
+                Systems.get(ServerSeekerSystem.class).discordUsername = discordUsername;
+                Systems.get(ServerSeekerSystem.class).discordAvatarUrl = discordAvatarUrl;
+
+                stream = Http.get(discordAvatarUrl).sendInputStream();
+            }
+            avatar = ImageIO.read(stream);
         } catch (IOException e) {
             e.printStackTrace();
             return;
