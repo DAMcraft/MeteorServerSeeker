17078d673c02481467e0bc3209e0a6d6b7e1c5fc
DAM <43420467+DAMcraft@users.noreply.github.com>
2023-08-05 11:06:10 +0200

    Version 2.0.0
    - Added BungeeSpoof, how to use: https://www.youtube.com/watch?v=a-uOyOGlivg

--- src/main/java/de/damcraft/serverseeker/ServerSeeker.java
+++ src/main/java/de/damcraft/serverseeker/ServerSeeker.java
@@ -1,7 +1,11 @@
 package de.damcraft.serverseeker;
 
 import com.mojang.logging.LogUtils;
+import de.damcraft.serverseeker.modules.BungeeSpoof;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
+import meteordevelopment.meteorclient.systems.modules.Category;
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import net.minecraft.item.*;
 import org.slf4j.Logger;
 
 public class ServerSeeker extends MeteorAddon {
@@ -12,14 +16,21 @@ public class ServerSeeker extends MeteorAddon {
     add(new Server database with around 1.000.000 servers!())
     add(new Over 40.000.000 players tracked!())
     add(new Search for ANY server you want!())
+    add(new Join misconfigured BungeeCord backends with any name you want!())
      */
     public static final Logger LOG = LogUtils.getLogger();
+    public static final Category CATEGORY = new Category("ServerSeeker", Items.SPYGLASS.getDefaultStack());
 
     @Override
     public void onInitialize() {
         LOG.info("Loaded the ServerSeeker addon!");
+        Modules.get().add( new BungeeSpoof() );
     }
 
+    @Override
+    public void onRegisterCategories() {
+        Modules.registerCategory(CATEGORY);
+    }
 
     @Override
     public String getPackage() {
---
+++ src/main/java/de/damcraft/serverseeker/modules/BungeeSpoof.java
@@ -0,0 +1,23 @@
+package de.damcraft.serverseeker.modules;
+
+import de.damcraft.serverseeker.ServerSeeker;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
+import meteordevelopment.meteorclient.settings.StringSetting;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+
+public class BungeeSpoof extends Module {
+    SettingGroup sgGeneral = settings.getDefaultGroup();
+
+    public Setting<String> spoofedAddress = sgGeneral.add(new StringSetting.Builder()
+        .name("spoofed-address")
+        .description("The spoofed IP address that will be sent to the server.")
+        .defaultValue("127.0.0.1")
+        .build()
+    );
+
+    public BungeeSpoof() {
+        super(ServerSeeker.CATEGORY, "BungeeSpoof", "Allows you to join servers with an exposed bungeecord backend.");
+    }
+}
