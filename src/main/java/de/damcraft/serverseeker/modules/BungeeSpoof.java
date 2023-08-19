39182a8c9f2db01b3ce8d318d322b9a4e6eafcb7
DAM <43420467+DAMcraft@users.noreply.github.com>
2023-08-19 11:14:52 +0200

    Moved everything into onPacketSend. No clue why I didn't do that from the beginning.

--- src/main/java/de/damcraft/serverseeker/modules/BungeeSpoof.java
+++ src/main/java/de/damcraft/serverseeker/modules/BungeeSpoof.java
@@ -1,10 +1,19 @@
 package de.damcraft.serverseeker.modules;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import de.damcraft.serverseeker.ServerSeeker;
+import de.damcraft.serverseeker.mixin.HandshakeC2SAccessor;
+import meteordevelopment.meteorclient.MeteorClient;
+import meteordevelopment.meteorclient.events.packets.PacketEvent;
 import meteordevelopment.meteorclient.settings.Setting;
 import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.settings.StringSetting;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.utils.network.Http;
+import meteordevelopment.orbit.EventHandler;
+import net.minecraft.network.packet.Packet;
+import net.minecraft.network.packet.c2s.handshake.HandshakeC2SPacket;
 
 
 public class BungeeSpoof extends Module {
@@ -20,5 +29,34 @@ public class BungeeSpoof extends Module {
 
     public BungeeSpoof() {
         super(ServerSeeker.CATEGORY, "BungeeSpoof", "Allows you to join servers with an exposed bungeecord backend.");
+        MeteorClient.EVENT_BUS.subscribe(new Listener());
+    }
+
+    private class Listener {
+        @EventHandler
+        private void onPacketSend(PacketEvent.Send event) {
+            if (!isActive()) return;
+            if (!(event.packet instanceof HandshakeC2SPacket)) return;
+            HandshakeC2SAccessor packet = (HandshakeC2SAccessor) event.packet;
+            if (packet.getNetworkState().getId() != 2) return;
+            ServerSeeker.LOG.info("Spoofing bungeecord handshake packet");
+            String spoofedUUID = mc.getSession().getUuid();
+
+            String URL = "https://api.mojang.com/users/profiles/minecraft/" + mc.getSession().getUsername();
+
+            Http.Request request = Http.get(URL);
+            String response = request.sendString();
+            if (response != null) {
+                Gson gson = new Gson();
+
+                JsonObject jsonObject = gson.fromJson(response, JsonObject.class);
+
+                if (jsonObject != null && jsonObject.has("id")) {
+                    spoofedUUID = jsonObject.get("id").getAsString();
+                }
+            }
+
+            packet.setAddress(packet.getAddress() + "\u0000" + spoofedAddress.get() + "\u0000" + spoofedUUID);
+        }
     }
 }
